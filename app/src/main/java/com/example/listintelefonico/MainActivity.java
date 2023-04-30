6369ccb96ff3eb8d7c9f78590cbaf8f45fccf252
Nia <88581938+NiaD95@users.noreply.github.com>
2023-04-30 20:22:27 +0200

    agrrgarcion de funcioens a los elementos

--- app/src/main/java/com/example/listintelefonico/MainActivity.java
+++ app/src/main/java/com/example/listintelefonico/MainActivity.java
@@ -4,13 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listContactos;
+    private Button bagregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listContactos = findViewById(R.id.lvListaContactos);
+        bagregar = findViewById(R.id.bAgregar);
     }
 
     public void onAgregar(View view) {
