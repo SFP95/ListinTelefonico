6779760cca0494e76ca2dbf0ca3647aeed496f8b
Nia <88581938+NiaD95@users.noreply.github.com>
2023-04-30 19:57:01 +0200

    creacion visual activity2

--- app/src/main/java/com/example/listintelefonico/MainActivity2.java
+++ app/src/main/java/com/example/listintelefonico/MainActivity2.java
@@ -3,6 +3,7 @@ package com.example.listintelefonico;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void onCancelar(View view) {
+    }
+
+    public void onAceptar(View view) {
     }
 }
