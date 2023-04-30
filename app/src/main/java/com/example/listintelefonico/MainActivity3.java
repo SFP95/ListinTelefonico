a3da290f5320cb9ec8732eb896c4ca2d3eb0a16f
Nia <88581938+NiaD95@users.noreply.github.com>
2023-04-30 20:00:25 +0200

    creacion visual activity3

--- app/src/main/java/com/example/listintelefonico/MainActivity3.java
+++ app/src/main/java/com/example/listintelefonico/MainActivity3.java
@@ -3,6 +3,7 @@ package com.example.listintelefonico;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+    }
+
+    public void onBorrar(View view) {
+    }
+
+    public void onAceptar(View view) {
+    }
+
+    public void onCancelar(View view) {
     }
 }
